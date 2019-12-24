d74ba9c03c63802f3b5cd03933dc63a299406603
jobiols <jorge.obiols@gmail.com>
2019-12-24 16:47:24 -0300

    [FIX] version funcional, falta revisar las cuentas.

--- l10n_py/__manifest__.py
+++ l10n_py/__manifest__.py
@@ -22,6 +22,7 @@ Install paraguayan chart of accounts.
         'demo/account_bank_statement_demo.xml',
         'demo/account_invoice_demo.xml',
         'demo/account_reconcile_model.xml',
+        'data/account_chart_template_data.xml',
     ],
     'uninstall_hook': 'uninstall_hook',
 }
