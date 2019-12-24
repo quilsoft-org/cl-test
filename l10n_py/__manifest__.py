a917c82953e193e1ce484a83730ad9a07a0ed7f1
jobiols <jorge.obiols@gmail.com>
2019-12-24 14:43:24 -0300

    [ADD] l10n_py

---
+++ l10n_py/__manifest__.py
@@ -0,0 +1,27 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Paraguayan - Accounting',
+    'version': '13.0.0.0.0',
+    'category': 'Localization',
+    'description': """
+Paraguayan accounting chart in Odoo.
+====================================
+
+Install paraguayan chart of accounts.
+    """,
+    'depends': [
+        'account',
+    ],
+    'data': [
+        'data/l10n_generic_coa.xml',
+        'data/account.account.template.csv',
+        'data/l10n_generic_coa_post.xml',
+    ],
+    'demo': [
+        'demo/account_bank_statement_demo.xml',
+        'demo/account_invoice_demo.xml',
+        'demo/account_reconcile_model.xml',
+    ],
+    'uninstall_hook': 'uninstall_hook',
+}
