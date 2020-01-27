13927f3ce9625e191d26ff76003103db3c139fee
jobiols <jorge.obiols@gmail.com>
2020-01-27 18:02:44 -0300

    [FIX] cambiar de repo

--- test13_default/__manifest__.py
+++ test13_default/__manifest__.py
@@ -37,7 +37,7 @@
     'repos': [
         {'usr': 'jobiols', 'repo': 'cl-test', 'branch': '13.0'},
         {'usr': 'jobiols', 'repo': 'odoo-addons', 'branch': '13.0'},
-        {'usr': 'TecnoproPy', 'repo': 'odoo-paraguay', 'branch': '13.0'},
+        {'usr': 'jobiols', 'repo': 'odoo-paraguay', 'branch': '13.0'},
     ],
     'docker': [
         {'name': 'odoo', 'usr': 'jobiols', 'img': 'odoo-ent', 'ver': '13.0e'},
