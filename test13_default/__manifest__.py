93dbda26f0c86b14ab1eba0f6939d2a531fa2308
jobiols <jorge.obiols@gmail.com>
2020-02-21 14:05:14 -0300

    [REM] stock

--- test13_default/__manifest__.py
+++ test13_default/__manifest__.py
@@ -50,7 +50,6 @@
         {'usr': 'ingadhoc', 'repo': 'aeroo_reports', 'branch': '13.0'},
         {'usr': 'ingadhoc', 'repo': 'sale', 'branch': '13.0'},
         {'usr': 'ingadhoc', 'repo': 'product', 'branch': '13.0'},
-        {'usr': 'ingadhoc', 'repo': 'stock', 'branch': '13.0'},
         {'usr': 'ingadhoc', 'repo': 'account-invoicing', 'branch': '13.0'},
 
         {'usr': 'oca', 'repo': 'partner-contact', 'branch': '13.0'},
