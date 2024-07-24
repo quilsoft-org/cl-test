1096080fbeb9d8fb7c3f07a3e58f2e3752e50041
Jorge Obiols <jorge.obiols@gmail.com>
2024-07-24 17:33:27 -0300

    [FIX] test17

--- test17_default/__manifest__.py
+++ test17_default/__manifest__.py
@@ -1,21 +1,8 @@
 ##############################################################################
 #
-#    Copyright (C) 2022  jeo Software  (http://www.jeosoft.com.ar)
+#    Copyright (C) 2024
 #    All Rights Reserved.
 #
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your optiogitn) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#   le agregamos esto
 ##############################################################################
 
 {
@@ -23,7 +10,7 @@
     "version": "17.0.1.0.0",
     "category": "Tools",
     "summary": "Test for v17 CE",
-    "author": "jeo Software",
+    "author": "Quilsoft",
     "website": "http://github.com/jobiols/cl-test",
     "license": "AGPL-3",
     "depends": [],
@@ -39,7 +26,8 @@
     ],
     "port": "8069",
     "git-repos": [
-        "https://github.com/jobiols/cl-test.git",
+        "git@github.com:quilsoft-org/cl-test.git",
+        "git@github.com:quilsoft-org/addons-private.git"
     ],
     # list of images to use in the form 'name image-url'
     "docker-images": [
