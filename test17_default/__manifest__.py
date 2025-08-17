7180199e37737012b4fdeaa346cb7591ac9bdf76
Jorge Obiols <jorge.obiols@gmail.com>
2025-08-17 15:18:10 -0300

    [WIP]

--- test17_default/__manifest__.py
+++ test17_default/__manifest__.py
@@ -27,6 +27,7 @@
     "port": "8069",
     "git-repos": [
         "git@github.com:quilsoft-org/cl-test.git",
+        "git@github.com:quilsoft-org/addons-private.git",
     ],
     # list of images to use in the form 'name image-url'
     "docker-images": [
