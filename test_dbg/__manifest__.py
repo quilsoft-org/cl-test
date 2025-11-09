737fb9892db3ef7b0d50b6b75728af9ae6ef7809
Jorge Obiols <jorge.obiols@gmail.com>
2025-11-09 17:31:43 -0300

    [ADD] test_dbg

---
+++ test_dbg/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    "name": "test_debug",
+    "version": "17.0.1.0.0",
+    "category": "Tools",
+    "summary": "Test debug for v16 CE",
+    "author": "jeo Software",
+    "website": "http://github.com/jobiols/cl-test",
+    "license": "AGPL-3",
+    "depends": ['fleet'],
+    "installable": True,
+}
